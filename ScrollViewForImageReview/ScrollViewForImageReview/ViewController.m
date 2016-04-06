4438b69319e95c9ff2f9a2549e774c002931b774
susu <28025920@qq.com>
2016-04-06 15:56:18 +0800

    1.代码创建scrollview
    2.加入懒加载方法
    3.实现内部图片的放大缩小

--- ScrollViewForImageReview/ScrollViewForImageReview/ViewController.m
+++ ScrollViewForImageReview/ScrollViewForImageReview/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@interface ViewController ()<UIScrollViewDelegate>
+@property (strong, nonatomic)UIScrollView *scrollView;
 @property(strong,nonatomic)UIImageView* imageView;
 @property(strong,nonatomic)UIImage* image;
 @end
@@ -23,6 +23,7 @@
     }
     return _imageView;
 }
+//重写image的setter方法
 -(void)setImage:(UIImage *)image{
     _image=image;
     //设置图像内容
@@ -31,11 +32,23 @@
     [self.imageView sizeToFit];
     
 }
+-(UIScrollView *)scrollView{
+    if (_scrollView==nil) {
+        _scrollView=[[UIScrollView alloc]initWithFrame:self.view.bounds];
+        _scrollView.minimumZoomScale=0.2;
+        _scrollView.maximumZoomScale=1.5;
+        _scrollView.delegate=self;
+        [self.view addSubview:_scrollView];
+    }
+    return _scrollView;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.image=[UIImage imageNamed:@"minion"];
     self.scrollView.contentSize=self.imageView.bounds.size;
 }
-
+-(nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return  self.imageView;
+}
 
 @end
